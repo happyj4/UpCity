7740a546b5ebb7416835f0136f57962247bdc7ec
NureKoriakinZakhar <zakhar.koriakin@nure.ua>
2025-03-25 13:42:30 +0200

    Добавлен экран входа в аккаунт, шрифты, иконки и тд

--- AndroidApp/app/src/main/java/com/example/upcity/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/example/upcity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.upcity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
