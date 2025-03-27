daba250addd30ed73353c4eaf191a06cf1e01179
NureKoriakinZakhar <zakhar.koriakin@nure.ua>
2025-03-27 18:58:48 +0200

    Сделана страница подачи заяв

---
+++ AndroidApp/app/src/main/java/com/example/upcity/CreateApplicationPage.java
@@ -0,0 +1,32 @@
+package com.example.upcity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class CreateApplicationPage extends AppCompatActivity {
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_application_page);
+
+        Button HomeButton = findViewById(R.id.HomeButton);
+        ShapeableImageView PhotoButton = findViewById(R.id.PhotoButton);
+
+        menu = new Menu();
+        PhotoButton.setOnClickListener(view -> menu.showPopupMenu(view, CreateApplicationPage.this));
+
+        HomeButton.setOnClickListener(view -> {
+            Intent intent = new Intent(CreateApplicationPage.this, HomePageActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();
+        });
+    }
+}
--- AndroidApp/app/src/main/java/com/example/upcity/HomePageActivity.java
+++ AndroidApp/app/src/main/java/com/example/upcity/HomePageActivity.java
@@ -1,6 +1,10 @@
 package com.example.upcity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,24 +20,31 @@ import java.util.List;
 public class HomePageActivity extends AppCompatActivity {
     private Menu menu;
 
-    private PopupWindow popupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
+        ImageButton PlusButton = findViewById(R.id.PlusButton);
         RecyclerView MyList = findViewById(R.id.MyList);
         RecyclerView AllList = findViewById(R.id.AllList);
         ShapeableImageView PhotoButton = findViewById(R.id.PhotoButton);
+
+        menu = new Menu();
+        PhotoButton.setOnClickListener(view -> menu.showPopupMenu(view, HomePageActivity.this));
+
+        PlusButton.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePageActivity.this, CreateApplicationPage.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();
+        });
 
         LinearLayoutManager layoutManagerMyList = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         MyList.setLayoutManager(layoutManagerMyList);
 
         GridLayoutManager gridLayoutManagerAllList = new GridLayoutManager(this, 2);
         AllList.setLayoutManager(gridLayoutManagerAllList);
-
-        menu = new Menu();
-        PhotoButton.setOnClickListener(view -> menu.showPopupMenu(view, HomePageActivity.this));
 
         List<Application> applicationList = new ArrayList<>();
         applicationList.add(new Application(1, "Заява 1", "01.01.2022", 123));
