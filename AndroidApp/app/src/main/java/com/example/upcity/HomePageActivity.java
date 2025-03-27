7d48f3c479a96ed2582f4410d16633e36cc1a6d4
NureKoriakinZakhar <zakhar.koriakin@nure.ua>
2025-03-27 17:47:53 +0200

    Добавлено поп ап меню

--- AndroidApp/app/src/main/java/com/example/upcity/HomePageActivity.java
+++ AndroidApp/app/src/main/java/com/example/upcity/HomePageActivity.java
@@ -1,23 +1,22 @@
 package com.example.upcity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.imageview.ShapeableImageView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomePageActivity extends AppCompatActivity {
-    boolean isMenuOpen = false;
+    private Menu menu;
 
+    private PopupWindow popupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +24,16 @@ public class HomePageActivity extends AppCompatActivity {
 
         RecyclerView MyList = findViewById(R.id.MyList);
         RecyclerView AllList = findViewById(R.id.AllList);
-        ImageButton PhotoButton = findViewById(R.id.PhotoButton);
+        ShapeableImageView PhotoButton = findViewById(R.id.PhotoButton);
 
         LinearLayoutManager layoutManagerMyList = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         MyList.setLayoutManager(layoutManagerMyList);
 
         GridLayoutManager gridLayoutManagerAllList = new GridLayoutManager(this, 2);
         AllList.setLayoutManager(gridLayoutManagerAllList);
+
+        menu = new Menu();
+        PhotoButton.setOnClickListener(view -> menu.showPopupMenu(view, HomePageActivity.this));
 
         List<Application> applicationList = new ArrayList<>();
         applicationList.add(new Application(1, "Заява 1", "01.01.2022", 123));
--- AndroidApp/app/src/main/java/com/example/upcity/Menu.java
+++ AndroidApp/app/src/main/java/com/example/upcity/Menu.java
@@ -1,15 +1,47 @@
 package com.example.upcity;
 
-import android.os.Bundle;
-import android.view.ViewGroup;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class Menu {
 
-public class Menu extends AppCompatActivity {
+    private PopupWindow popupWindow;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
+    public void showPopupMenu(View anchorView, Context context) {
+        View popupView = LayoutInflater.from(context).inflate(R.layout.menu, null);
+
+        popupWindow = new PopupWindow(popupView, 490, LinearLayout.LayoutParams.WRAP_CONTENT, true);
+
+        popupWindow.setElevation(7);
+        popupWindow.showAsDropDown(anchorView, -390, 50);
+
+        popupView.findViewById(R.id.EditProfileButton).setOnClickListener(v -> {
+            popupWindow.dismiss();
+            Toast.makeText(context, "Редагувати профіль", Toast.LENGTH_SHORT).show();
+        });
+
+        popupView.findViewById(R.id.MyApplicationButton).setOnClickListener(v -> {
+            popupWindow.dismiss();
+            Toast.makeText(context, "Мої звернення", Toast.LENGTH_SHORT).show();
+        });
+
+        popupView.findViewById(R.id.AllApplicationButton).setOnClickListener(v -> {
+            popupWindow.dismiss();
+            Toast.makeText(context, "Всі звернення", Toast.LENGTH_SHORT).show();
+        });
+
+        popupView.findViewById(R.id.PremiumButton).setOnClickListener(v -> {
+            popupWindow.dismiss();
+            Toast.makeText(context, "Преміум-підписка", Toast.LENGTH_SHORT).show();
+        });
+
+        popupView.findViewById(R.id.ExitButton).setOnClickListener(v -> {
+            popupWindow.dismiss();
+            Toast.makeText(context, "Вийти", Toast.LENGTH_SHORT).show();
+        });
     }
 }
