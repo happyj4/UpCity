1238375f1cbfecb32ec2b32dd57473c9cce558fe
NureKoriakinZakhar <zakhar.koriakin@nure.ua>
2025-03-28 14:16:17 +0200

    Сделаны сообщения об успехе

--- AndroidApp/app/src/main/java/com/example/upcity/CreateApplicationPage.java
+++ AndroidApp/app/src/main/java/com/example/upcity/CreateApplicationPage.java
@@ -18,12 +18,24 @@ public class CreateApplicationPage extends AppCompatActivity {
 
         Button HomeButton = findViewById(R.id.HomeButton);
         ShapeableImageView PhotoButton = findViewById(R.id.PhotoButton);
+        Button CreateApplicationButton = findViewById(R.id.CreateApplicationButton);
 
         menu = new Menu();
         PhotoButton.setOnClickListener(view -> menu.showPopupMenu(view, CreateApplicationPage.this));
 
         HomeButton.setOnClickListener(view -> {
             Intent intent = new Intent(CreateApplicationPage.this, HomePageActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();
+        });
+
+        CreateApplicationButton.setOnClickListener(view -> {
+            Intent intent = new Intent(CreateApplicationPage.this, MessagePage.class);
+
+            intent.putExtra("name", "Звернення Створено!");
+            intent.putExtra("description", "Впродовж 24 годин воно буде опрацьоване");
+
             startActivity(intent);
             overridePendingTransition(0, 0);
             finish();
