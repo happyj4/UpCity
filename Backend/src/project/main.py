6e45fa06da9fa76297b84c5f3a37cf913c8362dd
Serhii Kozlov <dinogithub56@gmail.com>
2025-05-13 22:25:45 +0300

    додав Cors

--- Backend/src/project/main.py
+++ Backend/src/project/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from project.api import utility_company, user, authentication, image, application
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # разрешить всем источникам (для общего доступа)
+    allow_credentials=True,
+    allow_methods=["*"],  # разрешить все методы: GET, POST, PUT, DELETE и т.д.
+    allow_headers=["*"],  # разрешить любые заголовки
+)
 
 app.mount("/images", StaticFiles(directory="/var/www/myapp/uploads/images"), name="images")
 
