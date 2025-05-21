57b0f07d0f2f7d8466c67d0457381468982a1fca
kerio1 <he4toki@gmail.com>
2025-05-21 21:10:46 +0300

    Додано заголовок сторінки списку КП

---
+++ Web/components/page6/start.jsx
@@ -0,0 +1,12 @@
+
+
+export function Start() {
+  return (
+    <div className="px-25 pt-45">
+      <h1 className="text-[#3A3A3A] text-4xl font-semibold mb-10">
+        Список комунальних підприємств
+      </h1>
+      
+    </div>
+  );
+}
